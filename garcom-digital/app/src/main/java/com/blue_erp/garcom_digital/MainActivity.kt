c3fe3c2503e91735924620d0ad43f489b42ad3e2
Elielton <elieltonramos14@gmail.com>
2026-02-20 20:46:43 -0300

    Iniciando Setup do Aplicativo de Garcom

---
+++ garcom-digital/app/src/main/java/com/blue_erp/garcom_digital/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.blue_erp.garcom_digital
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.blue_erp.garcom_digital.ui.theme.GarcomdigitalTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GarcomdigitalTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GarcomdigitalTheme {
+        Greeting("Android")
+    }
+}
