1c9571714558645e5c7026cd4e985310fc5727c3
Elielton <elieltonramos14@gmail.com>
2026-07-15 12:19:00 -0300

    refactor: :zap: Atualizando envolriment do frontend

--- frontend/src/app/core/services/environment.ts
+++ frontend/src/app/core/services/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: true,
   // apiUrl: 'http://localhost:3000' // teste dev
-  apiUrl: 'http://192.168.0.251:3000', // casa do lago local
+  apiUrl: 'http://192.168.0.219:3000', // casa do lago local
   // apiUrl: 'http://100.90.243.105:3000', // casa do lago VPN
   // apiUrl: 'https://blue-erp-api.portfoliostudio.cfd', // servidor nuvem
 };
